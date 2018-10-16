f89311580d3fa73de272939eb59ce97bfbc5a9d9
Student X <sweewen45@gmail.com>
2018-10-16 15:07:19 +0800

    set relationships among models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :category, optional: true
+  has_many :comments, dependent: :destroy
+  has_many :collects, dependent: :destroy
+  has_many :collectors, through: :collects, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :comments, dependent: :restrict_with_error
+  has_many :collects, dependent: :destroy
+  has_many :collected_posts, through: :collects, source: :post
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inviters, through: :inverse_friendships, source: :user       
+
   def admin?
     self.role == "admin"
   end
