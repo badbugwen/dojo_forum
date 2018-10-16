4d373d3a0decff7e6f59befe2f69bc35781d610c
Student X <sweewen45@gmail.com>
2018-10-16 18:19:19 +0800

    add category#show view and action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,31 @@
 class CategoriesController < ApplicationController
-  def index
-    @categories = Category.all
-    @category = Category.find(params[:id])
-    @posts = Post.all.order(created_at: :desc)
-  end
+  before_action :set_category
 
   def show
+    @categories = Category.all
+    @posts = Post.all.order(comments_count: :desc).page(params[:page]).per(20) 
+  end
+
+  def last_replied
+    ids = []
+    Comment.all.order(created_at: :asc).each do |comment|
+      ids << comment.post.id
+    end
+    ids.uniq
+
+    @posts = Post.joins(:comments).group("created_at").order(created_at: :desc).page(params[:page]).per(20) 
+    @categories = Category.all
+  end
+
+  def most_viewed
+    @posts = Post.order(viewed_count: :desc).page(params[:page]).per(20)
+    @categories = Category.all
+  end
+
+  private
+
+  def set_category
     @category = Category.find(params[:id])
   end
+
 end
