56086ee21d23007d68135356545f8ab36c8bc96f
Student X <sweewen45@gmail.com>
2018-10-24 19:32:55 +0800

    dd api post show/create/update/destroy but have problem with current_user

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,13 +1,66 @@
 class Api::V1::PostsController < ApiController
-
   def index
     @posts = Post.all
-    render json: {
-      data: @posts
-    }
+    render json:  @posts
   end
 
   def show
-    
+    @post = Post.find_by(id: params[:id])
+    if !@post
+      render json: {
+        message: "Can't find the post!",
+        status: 400
+      }  
+    else
+      render json: @post
+      @post.increase_view
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.status = (params[:commit] == "Draft") ? true : false
+    if @post.save
+      create_relation
+      render json: {
+        message: "Post was successfully created",
+        result: @post
+      }
+    else
+      render json:{
+        message: @post.errors
+      }
+    end
+  end
+
+  def update
+    @post.status = (params[:commit] == "Draft") ? true : false
+    if @post.update(post_params)
+      create_relation
+      render json:{
+        message: "Post was successfully updated",
+        result: @post
+      }
+      return
+    else
+      render jason:{
+      message: @post.errors
+      }
+    end
+  end 
+
+  def destroy
+    if current_user == @post.user || current_user.admin?
+      @post.destroy
+      render json: {
+        message: "Post was successfully deleted"
+      }
+    end
+  end 
+
+  private
+  def post_params
+    params.permit(:title, :image, :content, :seem, :status)
   end
 end
